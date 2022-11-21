32144e5f5faaf6956f84882b7c83866a259a9ad4
bnun <85907829+bnunowo@users.noreply.github.com>
2022-11-20 22:23:42 -0600

    Add files via upload

---
+++ screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --rate 240 --pos 1169x120 --rotate normal --output DisplayPort-1 --mode 1920x1080 --rate 240 --pos 0x0 --rotate left --output DisplayPort-2 --off --output HDMI-A-0 --off 
